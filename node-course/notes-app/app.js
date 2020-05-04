7df54fe64f6f92dba929f7fe58603944eb73f6d9
Adam Golec <algolec1990@yahoo.com>
2020-05-04 19:05:06 -0400

    add and remove on notes

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,17 +1,12 @@
 const chalk = require('chalk')
 const getNotes = require ('./notes.js')
 
-const message = getNotes();
-const successMsg = 'Success!'
-console.log(message)
+const command = process.argv[2]
+console.log(process.argv)
 
-console.log(chalk.green(successMsg))
-/*
-use the chalk library in your project.
-
-1. install 2.4.1 version of chalk.
-2. Load chalk into app.js
-3. Use this to print the sting "success!" to the console in green.
-4. Test your work.
-
-*/
+if(command === 'add' ) {
+    console.log('Adding node')
+}
+else if(command === 'remove') {
+    console.log('removing note')
+}
