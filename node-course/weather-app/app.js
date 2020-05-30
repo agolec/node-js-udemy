0a3a6c6c95654cb248b2f21a12449fa3621467f0
Adam Golec <algolec1990@yahoo.com>
2020-05-30 18:05:22 -0400

    added error handling to app

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -1,7 +1,56 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=531015d3106fd3263ae1028b52614cd4&query=9.916989,78.131406'
+/*const url = 'http://api.weatherstack.com/current?access_key=531015d3106fd3263ae1028b52614cd4&query=34.0522,-118.2437&units=f'
 
-request({ url: url }, (error, response) => {
-   console.log(response)
+request({ url: url, json: true }, (error, response) => {
+   //should take in response in celcius from response.body.current.temperature and return it's fahrenheit equivalent.
+   if(error){
+      console.log('unable to connect to the weather service\n\n')
+   } else if(response.body.error){
+      console.log('unable to find location.')
+      console.log('Error code is: ' + response.body.error.code)
+      console.log('Error code description is: ' + response.body.error.info)
+   }
+   
+   else{
+      console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + " degrees out gathered from a station in " + 
+      response.body.location.country + ". It feels like " + response.body.current.feelslike + " outdoors.\n\n")
+   }
+
+  
+
 })
+*/
+
+// Geocoding
+// Address -> Latitude/Longitude -> Weather
+
+const geocodingURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Kenya.json?access_token=pk.eyJ1IjoiYWdvbGVjIiwiYSI6ImNrYWx1MWNseDE1dHoycmw2MXE1bmNlZWkifQ.98L-uyyu71PEHtQ-JYwaNg&limit=1'
+
+
+request({ url: geocodingURL, json: true }, (error, response) => {
+
+  
+
+   if(error){
+
+      console.log('unable to connect to the weather service\n\n')
+
+   } else if(response.body.features.length === 0) {
+
+      console.log('Location did not return results. Try another city or country name.\n\n')
+      
+   }
+   else {
+
+      const latitude = response.body.features[0].center[1]
+      const longitude = response.body.features[0].center[0]
+      const name = response.body.features[0].place_name
+      console.log('lat is ' + latitude + ' and long is ' + longitude )
+      console.log('The place searched is ' + name + '. The coordinates of ' + name + ' are: ' + latitude + ' and ' + longitude )
+
+   }
+})
+
+
+
