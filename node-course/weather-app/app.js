98ea0dca55f1f5c478009e0048e14b60eef77423
Adam Golec <algolec1990@yahoo.com>
2020-05-30 21:34:21 -0400

    made code into abstracted callback functions

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -1,4 +1,5 @@
-const request = require('request')
+
+const geocode = require('./utils/geocode')
 
 /*const url = 'http://api.weatherstack.com/current?access_key=531015d3106fd3263ae1028b52614cd4&query=34.0522,-118.2437&units=f'
 
@@ -25,33 +26,39 @@ request({ url: url, json: true }, (error, response) => {
 // Geocoding
 // Address -> Latitude/Longitude -> Weather
 
-const geocodingURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Kenya.json?access_token=pk.eyJ1IjoiYWdvbGVjIiwiYSI6ImNrYWx1MWNseDE1dHoycmw2MXE1bmNlZWkifQ.98L-uyyu71PEHtQ-JYwaNg&limit=1'
+// const geocodingURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Kenya.json?access_token=pk.eyJ1IjoiYWdvbGVjIiwiYSI6ImNrYWx1MWNseDE1dHoycmw2MXE1bmNlZWkifQ.98L-uyyu71PEHtQ-JYwaNg&limit=1'
 
-
-request({ url: geocodingURL, json: true }, (error, response) => {
+// request({ url: geocodingURL, json: true }, (error, response) => {
 
   
 
-   if(error){
+//    if(error){
 
-      console.log('unable to connect to the weather service\n\n')
+//       console.log('unable to connect to the weather service\n\n')
 
-   } else if(response.body.features.length === 0) {
+//    } else if(response.body.features.length === 0) {
 
-      console.log('Location did not return results. Try another city or country name.\n\n')
+//       console.log('Location did not return results. Try another city or country name.\n\n')
       
-   }
-   else {
+//    }
+//    else {
 
-      const latitude = response.body.features[0].center[1]
-      const longitude = response.body.features[0].center[0]
-      const name = response.body.features[0].place_name
-      console.log('lat is ' + latitude + ' and long is ' + longitude )
-      console.log('The place searched is ' + name + '. The coordinates of ' + name + ' are: ' + latitude + ' and ' + longitude )
+//       const latitude = response.body.features[0].center[1]
+//       const longitude = response.body.features[0].center[0]
+//       const name = response.body.features[0].place_name
+//       console.log('lat is ' + latitude + ' and long is ' + longitude )
+//       console.log('The place searched is ' + name + '. The coordinates of ' + name + ' are: ' + latitude + ' and ' + longitude )
 
-   }
+//    }
 
 
+// })
+
+
+
+geocode('New Delhi', (error, data) => {
+   console.log('Error:', error)
+   console.log('Data: ', data)
 })
 
 
