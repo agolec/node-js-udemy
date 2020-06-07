a4392a81ae48ba699ddc0ee68e5d9ede7f8a560a
Adam Golec <algolec1990@yahoo.com>
2020-06-07 17:39:43 -0400

    added handlebars npm library into the app

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -3,9 +3,16 @@ const express = require('express')
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname,'../public')
-const helpPage = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'weather app',
+        name: 'Andrew Mead'
+    })
+})
 
 app.get('/weather', (req,res) => {
     res.send({
