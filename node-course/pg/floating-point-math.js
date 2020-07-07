1095163ed17b5f3e45fd8e3b2e82f367b13ce7bb
Adam Golec <algolec1990@yahoo.com>
2020-07-07 12:55:36 -0400

    adding to sanity check what is going on

---
+++ node-course/pg/floating-point-math.js
@@ -0,0 +1,7 @@
+const a = 0.1
+const b = 0.2
+
+const c = a + b
+
+console.log('result is ' + c )
+console.log('result is ' + c) //adding
--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -1,17 +1,31 @@
-const path = require('path')
 const express = require('express')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname,'../public')
-const helpPage = path.join(__dirname, '../public')
 
-app.use(express.static(publicDirectoryPath))
+app.get('', (req, res) => {
+    res.send('<h1>Weather</h1>')    
+})
+
+app.get('/help', (req,res) => {
+    res.send([{
+        name:'Adam'
+    },{
+        name:'Sarah'
+    }])
+})
+
+//update routes of 'about' to have a large html title
+
+//update 'weather' to send back a JSON object with forecast and location strings.
+
+
+
+app.get('/about', (req,res) => {
+    res.send('about page')
+})
 
 app.get('/weather', (req,res) => {
-    res.send({
-        forecast: 'Sunny',
-        location: 'foo'
-    })
+    res.send('Weather Page')
 })
 app.listen(3000, () => { 
     console.log('Server is up on port 3000')
